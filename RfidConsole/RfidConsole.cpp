623c84e9144fca18173a43e15f6ec462f4472b8f
Stuart Hallows <stuart.hallows@gmail.com>
2024-12-23 21:10:53 +1030

    Initial commit

---
+++ RfidConsole/RfidConsole.cpp
@@ -0,0 +1,12 @@
+﻿// RfidConsole.cpp : Defines the entry point for the application.
+//
+
+#include "RfidConsole.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
